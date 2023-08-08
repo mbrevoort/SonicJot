8f56f79fe03140d38ddf061a1792d8cc9dc12b28
Mike Brevoort <mike.brevoort@gitpod.io>
2023-08-07 22:24:48 -0600

    Initial Commit

---
+++ QuickScribe/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  QuickScribe
+//
+//  Created by Mike Brevoort on 8/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuickScribe/QuickScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickScribeApp.swift
+//  QuickScribe
+//
+//  Created by Mike Brevoort on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct QuickScribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
